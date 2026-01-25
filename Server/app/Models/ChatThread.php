5f24c348ac4e9a1e88371bff6daa5ef04014ca58
okobb <okobrosli2@gmail.com>
2026-01-25 23:05:53 +0200

    Created the migrations and models needed for messages and history.

---
+++ Server/app/Models/ChatThread.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ChatThread extends Model
+{
+    protected $guarded = [];
+
+    protected $casts = [
+        'metadata' => 'array',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(ChatMessage::class);
+    }
+}
